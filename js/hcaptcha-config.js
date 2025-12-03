15c38a273d0c001634af0af7039fa5abc4305bc0
ace1mbj-droid <ace1mbj@gmail.com>
2025-12-03 18:08:12 +0530

    chore: update hCaptcha site key

--- js/hcaptcha-config.js
+++ js/hcaptcha-config.js
@@ -1,7 +1,7 @@
 // hCaptcha configuration
 // Replace the placeholder site key with your production key from https://dashboard.hcaptcha.com/sites
 
-window.HCAPTCHA_SITE_KEY = window.HCAPTCHA_SITE_KEY || '38929996-78b6-499c-95a6-3aacdaae0ec4';
+window.HCAPTCHA_SITE_KEY = window.HCAPTCHA_SITE_KEY || 'edf8e53e-9be8-4c5a-8434-1eae72e636f2';
 window.HCAPTCHA_THEME = window.HCAPTCHA_THEME || 'light';
 window.HCAPTCHA_VERIFY_ENDPOINT = window.HCAPTCHA_VERIFY_ENDPOINT
 	|| (typeof SUPABASE_CONFIG !== 'undefined' ? `${SUPABASE_CONFIG.url}/functions/v1/verify-hcaptcha` : '');
