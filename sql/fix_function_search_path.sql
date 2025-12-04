7b3a302a0d213a4f227be09965d688098c1a0f35
ace1mbj-droid <ace1mbj@gmail.com>
2025-12-04 11:42:32 +0530

    Update: add SQL to remove unused function_name placeholder

--- sql/fix_function_search_path.sql
+++ sql/fix_function_search_path.sql
@@ -1,12 +1,15 @@
 -- Fix Function Search Path Mutable warnings
--- Set immutable search_path for security
+-- Set immutable search_path for security OR remove unused functions
 -- Run this in Supabase SQL Editor
 
--- 1. Fix cleanup_expired_sessions function
-ALTER FUNCTION public.cleanup_expired_sessions()
-SET search_path = public;
+-- 1. Fix cleanup_expired_sessions function (COMPLETED ✅)
+-- ALTER FUNCTION public.cleanup_expired_sessions()
+-- SET search_path = public;
 
--- 2. Fix function_name function (if it exists)
+-- 2. Remove placeholder function_name (appears to be a test function)
+DROP FUNCTION IF EXISTS public.function_name();
+
+-- If function_name is actually used, fix it instead with:
 -- ALTER FUNCTION public.function_name()
 -- SET search_path = public;
 
